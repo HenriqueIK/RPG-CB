27caa168338d312e7e48d041960edfe34743adf6
HenriqueIK <henriqueissaokato@gmail.com>
2026-05-07 18:46:18 -0300

    Adicionado efeito fade e arrumei a imagem de fundo

--- src/components/ImageSlides.tsx
+++ src/components/ImageSlides.tsx
@@ -1,29 +1,43 @@
 import { useEffect, useState } from "react"
 
 import img1 from "/assets/img/545821.jpg"
-import img2 from "/assets/img/1204665.jpg"
+import img2 from "/assets/img/1204665_01.jpg"
 import img3 from "/assets/img/1229131.jpg"
 import img4 from "/assets/img/1302137.jpg"
-// fui puxar a imagem 5 mas tava dando B.O então tenho quer ver depois ate la so festa
 
 const imagens = [img1, img2, img3, img4]
 
 export default function ImageSlider() {
   const [index, setIndex] = useState(0)
+  const [fade, setFade] = useState(true)
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setIndex((prev) => (prev + 1) % imagens.length)
-    }, 7000) // troca a cada 7 segundos
+
+      // começa fade out
+      setFade(false)
+
+      setTimeout(() => {
+        // troca imagem
+        setIndex((prev) => (prev + 1) % imagens.length)
+
+        // fade in
+        setFade(true)
+      }, 350) // efeito do fade dura 0,35 segundos
+
+    }, 10000) // a cada 10 segundos troca a imagem
 
     return () => clearInterval(interval)
   }, [])
 
   return (
-    <div className="h-full w-full">
+    <div className="h-screen w-full overflow-hidden bg-[#0d192b]">
       <img
         src={imagens[index]}
-        className="h-full w-full object-cover transition-opacity duration-1000"
+        alt="slide"
+        className={`h-full w-full object-cover transition-all duration-700 ease-in-out ${
+        fade ? "opacity-100 scale-100" : "opacity-0 scale-105"
+        }`}
       />
     </div>
   )
